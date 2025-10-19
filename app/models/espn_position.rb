161269c8fce21f76e18aefa3e79ec118543645f5
Zach Moses <zmoses93@gmail.com>
2025-10-18 21:39:23 -0700

    Error accidentally raised on EspnPosition.position_map when OF position

--- app/models/espn_position.rb
+++ app/models/espn_position.rb
@@ -8,7 +8,7 @@ class EspnPosition < ApplicationRecord
     return position if [ "1B", "2B", "3B", "SS", "C", "RP", "SP", "DH", "OF" ].include?(position)
 
     # ESPN doesn't distinguish between outfield positions
-    "OF" if [ "LF", "RF", "CF" ].include?(position)
+    return "OF" if [ "LF", "RF", "CF" ].include?(position)
 
     raise UnknownPositionError, "Unknown position: #{position}"
   end
