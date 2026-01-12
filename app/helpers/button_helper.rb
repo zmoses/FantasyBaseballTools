ea5eb09a2ecfcfa54ef2e901ca4a9e40303b42a8
Zach Moses <zmoses93@gmail.com>
2026-01-11 16:35:38 -0800

    Colors are more consistent

--- app/helpers/button_helper.rb
+++ app/helpers/button_helper.rb
@@ -12,9 +12,7 @@ module ButtonHelper
   end
 
   def button_base_classes
-    "px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 cursor-pointer " \
-    "hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 " \
-    "dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 " \
-    "dark:focus:ring-blue-500 dark:focus:text-white"
+    "px-4 py-2 text-sm font-normal text-white bg-slate-800 border border-slate-700 cursor-pointer " \
+    "hover:bg-slate-600 focus:ring-blue-500 focus:z-10 focus:ring-2"
   end
 end
