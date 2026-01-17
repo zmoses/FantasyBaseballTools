b35f33cdcb5a02a4863205356cce00f133587cd6
Zach Moses <zmoses93@gmail.com>
2026-01-17 13:40:54 -0800

    Table headers styles for sorting
    Still doesn’t functionally work though, that’s still to come.

--- app/helpers/draft_board_helper.rb
+++ app/helpers/draft_board_helper.rb
@@ -24,4 +24,10 @@ module DraftBoardHelper
 
     "background-color: hsl(#{hue}, #{saturation}%, #{lightness}%);"
   end
+
+  def chevron_down_icon(tailwind_color: "text-slate-700", tailwind_size: "size-5")
+    content_tag(:svg, class: "#{tailwind_color} #{tailwind_size}", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 20 20", fill: "currentColor", "data-slot": "icon", "aria-hidden": "true") do
+      content_tag(:path, nil, "fill-rule": "evenodd", d: "M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z", "clip-rule": "evenodd")
+    end
+  end
 end
