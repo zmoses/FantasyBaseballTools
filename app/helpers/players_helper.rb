9fa58c4888b2ce3d4e1b2b57343a1c4516665fb3
Zach Moses <zmoses93@gmail.com>
2026-02-10 16:32:39 -0800

    Positions added to table

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,6 +1,12 @@
 module PlayersHelper
-  def player_logo_and_name(player)
+  def player_logo_name_and_positions(player)
     logo = player.team ? image_tag("mlb_logos/#{player.team}.png", size: 30) : "(FA)"
-    safe_join([ content_tag(:span, logo, class: "mr-4 inline-block align-middle"), player.name ], " ")
+    positions = player.espn_positions.map(&:position).join(", ")
+
+    safe_join([
+      content_tag(:span, logo, class: "mr-4 inline-block align-middle"),
+      content_tag(:span, player.name, class: "font-semibold"),
+      content_tag(:span, positions, class: "italic ml-2")
+    ], " ")
   end
 end
