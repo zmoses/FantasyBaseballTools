134c36b2e398e5dc6b2cef6d997457c3bba8c6d0
Zach Moses <zmoses93@gmail.com>
2026-02-24 21:37:34 -0800

    ESPN positions only need to handle OF for now

--- app/models/espn_position.rb
+++ app/models/espn_position.rb
@@ -4,11 +4,12 @@ class EspnPosition < ApplicationRecord
   class UnknownPositionError < StandardError; end
 
   def self.position_map(position)
+    # ESPN doesn't distinguish between outfield positions in most leagues
+    # Will eventually need changed to handle league custom settings
+    return "OF" if [ "LF", "RF", "CF" ].include?(position)
+
     # Valid positions, return as-is
     return position if Espn::Constants::POSITIONS_BY_ID.values.include?(position)
-
-    # ESPN doesn't distinguish between outfield positions
-    return "OF" if [ "LF", "RF", "CF" ].include?(position)
 
     raise UnknownPositionError, "Unknown position: #{position}"
   end
