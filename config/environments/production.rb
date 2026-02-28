bbdb0f2decf64faab10a9b2a8162cbfd7a8bb168
Zach Moses <zmoses93@gmail.com>
2026-02-28 15:20:30 -0800

    friendship ended with solid cache, memory store is my friend now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  # Use in-process memory cache store.
+  config.cache_store = :memory_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
