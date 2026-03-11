7da4d288cceef3c4c517090322b9f2da014f6d6f
Zach Moses <zmoses93@gmail.com>
2026-03-11 10:33:28 -0700

    Fix migration for existing users

--- db/migrate/20260304225007_user_id_not_null_on_user_player.rb
+++ db/migrate/20260304225007_user_id_not_null_on_user_player.rb
@@ -1,5 +1,10 @@
 class UserIdNotNullOnUserPlayer < ActiveRecord::Migration[8.1]
-  def change
+  def up
+    UserPlayer.where(user_id: nil).delete_all
     change_column_null :user_players, :user_id, false
+  end
+
+  def down
+    change_column_null :user_players, :user_id, true
   end
 end
