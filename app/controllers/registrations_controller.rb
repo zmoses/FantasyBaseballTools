bcb478a6f1dc8e97bef98beb52c34c61bfde2759
Zach Moses <zmoses93@gmail.com>
2026-03-11 10:21:58 -0700

    Create user section added

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,23 @@
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access only: %i[ new create ]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to new_league_path, notice: "Account created! Set up your first league to get started."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :leagues
 
   resource :session
+  resource :registration, only: %i[ new create ]
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
